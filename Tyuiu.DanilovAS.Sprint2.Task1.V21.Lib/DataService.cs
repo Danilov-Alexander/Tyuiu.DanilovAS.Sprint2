6e4dfa1e86c9325e7924bc2f2f578f081e95828e
Tyuiu.DanilovAlexander <Tyuiu.DanilovAlexander@outlook.com>
2023-10-02 17:58:04 +0500

    Task1

---
+++ Tyuiu.DanilovAS.Sprint2.Task1.V21.Lib/DataService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using tyuiu.cources.programming.interfaces.Sprint2;
+
+namespace Tyuiu.DanilovAS.Sprint2.Task1.V21.Lib
+{
+    public class DataService : ISprint2Task1V21
+    {
+        public bool[] GetLogicOperations(int a, int b, int c, int d)
+        {
+            ///Написать программу из операций сравнений (==, !=, <, >, <=, >=, последовательность можно чередовать,
+            ///но использовать один раз в выражении) и логических операций (|, &, ||, &&, !, ^,
+            ///последовательность операций не должна нарушаться), а также арифметических выражений,
+            ///которая вернет логическую последовательность(массив): (False, False, False, False, True, False),
+            ///при a = 696, b = 987, c = 696, d = 155
+            bool[] temp = new bool[6];
+            temp[0] = (a < b) && (c < d) ; // False
+            temp[1] = (a < b) ^ (c > d) ; // False
+            temp[2] = (a > b) & (c < d); // False
+            temp[3] = (a > b) || (c < d);// False
+            temp[4] = (a < b) & (c > d); // True
+            temp[5] = (a ==b) || (c == d); //False
+            return temp;
+        }
+    }
+}
---
+++ Tyuiu.DanilovAS.Sprint2.Task1.V21/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.DanilovAS.Sprint2.Task1.V21.Lib;
+
+namespace Tyuiu.DanilovAS.Sprint2.Task1.V21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            int a = 696;
+            int b = 987;
+            int c = 696;
+            int d = 155;
+            bool[] res = new bool[6];
+            res = ds.GetLogicOperations(a, b, c, d);
+
+            Console.Title = "Спринт #2 | Выполнил: Данилов А. С. | ИСТНб-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Создания итогового решения по спринту                             *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #21                                                             *");
+            Console.WriteLine("* Выполнил: Данилов Александр Сергеевич | ИСТНб-23-1                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу из операций сравнений и логических операций          *");
+            Console.WriteLine("* а также арифметических выражений, которая вернет логическую             *");
+            Console.WriteLine("* последовательность, при a = 696, b = 987, c = 696, d = 155              *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("A = " + a);
+            Console.WriteLine("B = " + b);
+            Console.WriteLine("C = " + c);
+            Console.WriteLine("D = " + d);
+
+
+            Console.WriteLine();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            for (int i =0; i <6; i++)
+            {
+                Console.WriteLine(res[i]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
