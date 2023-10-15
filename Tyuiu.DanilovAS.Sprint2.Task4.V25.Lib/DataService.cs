f2e77fdd559f8466c202311f3ec95df7eea22c3c
Tyuiu.DanilovAlexander <Tyuiu.DanilovAlexander@outlook.com>
2023-10-15 16:44:39 +0500

    Task4

--- Tyuiu.DanilovAS.Sprint2.Task4.V25.Lib/DataService.cs
+++ Tyuiu.DanilovAS.Sprint2.Task4.V25.Lib/DataService.cs
@@ -12,7 +12,7 @@ namespace Tyuiu.DanilovAS.Sprint2.Task4.V25.Lib
     {
         public double Calculate(double x, double y)
         {
-            double z = x - 20 * 2 < y ? Math.Pow((1+(2/(Math.Pow(x,2)))), y) : y + Math.Pow(((x + 1) / (y + 2)), x);
+            double z = x - 20 * 2 < y/4 ? Math.Pow((1+(2/(Math.Pow(x,2)))), y) : y + Math.Pow(((x + 1) / (y + 2)), x);
             return Math.Round(z,3);
         }
     }
