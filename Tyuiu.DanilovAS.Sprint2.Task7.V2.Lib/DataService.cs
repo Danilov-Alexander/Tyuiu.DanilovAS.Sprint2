4421d14bbd88be477b24350c921efb9f70383a75
Tyuiu.DanilovAlexander <Tyuiu.DanilovAlexander@outlook.com>
2023-10-21 10:02:50 +0500

    Task7

--- Tyuiu.DanilovAS.Sprint2.Task7.V2.Lib/DataService.cs
+++ Tyuiu.DanilovAS.Sprint2.Task7.V2.Lib/DataService.cs
@@ -14,7 +14,7 @@ namespace Tyuiu.DanilovAS.Sprint2.Task7.V2.Lib
         {
 
             bool res;
-            if ((Math.Pow(x, 2) + Math.Pow(y, 2) <= 1) && ((y*x)/2)<0 && (x/(2*y)<0))
+            if ((Math.Pow(x, 2) + Math.Pow(y, 2) <= 1) && (y <= (x / 2)))
             {
                 res = true;
             }
